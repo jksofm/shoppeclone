69c6837275fc93a56c2e1be1b9bfb3e08c526577
jksofm <lequochuypy1998@gmail.com>
2024-02-24 16:50:28 +0700

    fix reload 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [
+    react({
+      include: '**/*.tsx'
+    }),
+    visualizer()
+  ],
   test: {
     environment: 'jsdom',
     setupFiles: path.resolve(__dirname, './vitest.setup.js')
